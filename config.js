dc7767810fdcf89085fb238ba84dd2f933fb0590
OK CITY <ookkcity@gmail.com>
2026-01-06 21:27:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    // 每次重新部署 GAS 後，只需要更新這裡的 ID
+    // 每次部署 GAS 新版本後，只需更新這裡的 ID
     API_URL: "https://script.google.com/macros/s/AKfycbzf5nq4HNJy-lzPy9BIDqm9ByR2dmTEu5wE_LItSPuIqRrkhe6b1hd7NwNakuryxEY2/exec"
 };
